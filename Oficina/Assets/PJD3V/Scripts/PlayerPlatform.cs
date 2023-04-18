9568d82ff87d73584e3bf2126e87185a4507c7cd
Renan Santos <renandsz@hotmail.com>
2023-04-18 14:09:57 -0300

    forcapulo

--- Oficina/Assets/PJD3V/Scripts/PlayerPlatform.cs
+++ Oficina/Assets/PJD3V/Scripts/PlayerPlatform.cs
@@ -6,6 +6,7 @@ public class PlayerPlatform : MonoBehaviour
 {
 
     public int velocidade;
+    public int forcaPulo;
 
 
     // Start is called before the first frame update
@@ -21,6 +22,14 @@ public class PlayerPlatform : MonoBehaviour
 
         Vector3 inputTeclado = new Vector3(x, 0, 0);
         transform.position += inputTeclado * velocidade * Time.deltaTime;
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("PRESSIONOU");
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * forcaPulo, ForceMode2D.Impulse);
+        }
         
+
     }
 }
