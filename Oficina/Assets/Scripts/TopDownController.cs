a9831a9e65ae5f4a0d7ed57c80c12b8e7252c5a8
Renan Santos <renandsz@hotmail.com>
2023-04-18 07:33:55 -0300

    script top down básico

---
+++ Oficina/Assets/Scripts/TopDownController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownController : MonoBehaviour
+{
+    public int velocidade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //pegando o input do teclado
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 inputTeclado = new Vector3(x, y, 0); //criou um vetor com os valores de input
+
+        //usando o input para mover o transform
+
+        transform.position += inputTeclado * velocidade * Time.deltaTime;
+
+    }
+}
