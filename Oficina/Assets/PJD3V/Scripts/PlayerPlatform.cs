52c0cdcbfb0c8cb0f85283c21df8ee672c32654c
Renan Santos <renandsz@hotmail.com>
2023-04-18 14:21:27 -0300

    sprite megaman com flip horizontal

--- Oficina/Assets/PJD3V/Scripts/PlayerPlatform.cs
+++ Oficina/Assets/PJD3V/Scripts/PlayerPlatform.cs
@@ -7,6 +7,8 @@ public class PlayerPlatform : MonoBehaviour
 
     public int velocidade;
     public int forcaPulo;
+    public bool noAr;
+    public SpriteRenderer visual;
 
 
     // Start is called before the first frame update
@@ -14,6 +16,22 @@ public class PlayerPlatform : MonoBehaviour
     {
         
     }
+
+
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        noAr = false;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        noAr = true;
+    }
+
+
+
 
     // Update is called once per frame
     void Update()
@@ -24,12 +42,24 @@ public class PlayerPlatform : MonoBehaviour
         transform.position += inputTeclado * velocidade * Time.deltaTime;
 
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !noAr)
         {
             Debug.Log("PRESSIONOU");
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * forcaPulo, ForceMode2D.Impulse);
         }
         
+        if(x < 0)
+        {
+            //olhando pra esquerda
+            visual.flipX = true;
+        }
+        else if(x > 0)
+        {
+            //olhando pra direita
+            visual.flipX = false;
+        }
+
+
 
     }
 }
